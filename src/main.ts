00f0517266c76282639c5d54bd0d879123c98a91
Ape Dev <admin@chainstride.capital>
2021-12-22 19:57:39 +0100

    enable cors for testing

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { PrismaService } from './services/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
   await app.listen(8000);
