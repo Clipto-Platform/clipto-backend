850ddbfd02e0be5b2a690f4d182c3e83e412d049
jonathanng1122 <jonathanng1122@gmail.com>
2022-01-14 17:41:19 -0600

    auth for update too

--- src/auth/web3.strategy.ts
+++ src/auth/web3.strategy.ts
@@ -4,14 +4,14 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class Web3Strategy extends PassportStrategy(Strategy) { //TODO(jonathanng) - make message signing be correct wording updating and creating requests
-  messageToBeSigned = 'I am onboarding to Clipto';
-
+  onboardingMessageToBeSigned = 'I am onboarding to Clipto';
+  updateMessageToBeSigned = 'I am updating my profile in Clipto';
   constructor() {
     super();
   }
 
   async validate(address: string, message: string): Promise<any> {
-    if (message !== this.messageToBeSigned) {
+    if (message !== this.onboardingMessageToBeSigned && message !== this.updateMessageToBeSigned) {
       throw new HttpException('Incorrect message signed!', HttpStatus.UNAUTHORIZED);
     }
     return { address };
