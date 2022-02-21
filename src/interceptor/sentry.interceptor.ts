20254dd6f23f1db7fc0b503aa228d8ef584bebab
atul <atulpatare99@gmail.com>
2022-02-21 18:31:50 +0530

    capturing all events

--- src/interceptor/sentry.interceptor.ts
+++ src/interceptor/sentry.interceptor.ts
@@ -1,4 +1,4 @@
-import { CallHandler, ExecutionContext, HttpException, Injectable, NestInterceptor } from '@nestjs/common';
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import * as Sentry from '@sentry/node';
 import { catchError, Observable, throwError } from 'rxjs';
 
@@ -7,11 +7,7 @@ export class SentryInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       catchError((exception) => {
-        if (exception instanceof HttpException && exception.getStatus() >= 500) {
-          Sentry.captureException(exception);
-        } else {
-          Sentry.captureException(exception);
-        }
+        Sentry.captureException(exception);
         return throwError(() => exception);
       }),
     );
